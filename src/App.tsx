8ecf470f6c9e4fab944bfe142938c7531dcd3653
winljm001 <819648983@qq.com>
2024-05-17 17:59:00 +0800

    feat: init

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import { TestApi } from "./services";
 
 function App() {
   const [count, setCount] = useState(0);
-
+  useEffect(() => {
+    new TestApi().findTestById(213);
+  }, []);
   return (
     <>
       <p className="read-the-docs">
